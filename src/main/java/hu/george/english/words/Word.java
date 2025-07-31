8d7322116b416b1fa6ca64d75b9f14d201539234
unknown <acsgyorgy.mail@gmail.com>
2025-07-31 16:26:41 +0200

    @JsonProperty, @Column annotations added, and now working the /create-word endpoint. i can add english and hungarian words to mysql.

--- src/main/java/hu/george/english/words/Word.java
+++ src/main/java/hu/george/english/words/Word.java
@@ -1,25 +1,28 @@
 package hu.george.english.words;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import lombok.Getter;
-import lombok.Setter;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.*;
+;
+import lombok.*;
 
 @Entity
 @Table(name = "word")
 @Getter
 @Setter
-
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class Word {
 
     @Id
     @GeneratedValue
     private int id;
 
+    @JsonProperty("englishWord")
+    @Column(name = "english_word")
     private String englishWord;
-
+    @JsonProperty("hungarianWord")
+    @Column(name = "hungarian_word")
     private String hungarianWord;
 
 }
--- src/main/java/hu/george/english/words/WordRepository.java
+++ src/main/java/hu/george/english/words/WordRepository.java
@@ -1,10 +1,12 @@
 package hu.george.english.words;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface WordRepository extends JpaRepository<Word, Integer> {
-
-
+    
 }
